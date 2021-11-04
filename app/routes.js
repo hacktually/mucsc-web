f09d19767f520f348c81a78610c68b034b025471
dealhl <62108986+dealhl@users.noreply.github.com>
2021-11-03 22:15:56 -0400

    Added files.

---
+++ app/routes.js
@@ -0,0 +1,24 @@
+module.exports = function(app){
+	
+	app.get('/', function(req,res){
+		res.send('hei!')
+	});
+	
+	// app.get('/add_data', function(req,res){
+	// 	conn.query("SELECT num_inputs FROM project_meta;", function(err, inputs){
+	// 		res.render('add_data.html', {inputs: inputs[0].num_inputs});
+	// 	});
+	// });
+
+	// app.post('/insert_data', function(req,res){
+	// 	console.log("Connection made.");
+	// 	//conn.query("INSERT INTO project_meta (project_name, num_inputs, meta_data) VALUES (?,?,?);", [req.body.col0, req.body.col1, req.body.col2]);
+	// });
+		
+
+	// app.get('/map_vis', function(req,res){
+	// 	res.render('map_vis.html');
+	// });
+
+
+};
